725f46563c04cc8fb6142576841b001279500fd4
diogotorres13 <104327677+diogotorres13@users.noreply.github.com>
2023-08-30 16:42:05 -0300

    Criando os teste

--- Teste/Relogio/RelogioTest.java
+++ Teste/Relogio/RelogioTest.java
@@ -4,11 +4,24 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class RelogioTest {
+public class RelogioTest {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+		@Test
+		public void testReiniciarMeiaNoite() {
+			Relogio relogio = new Relogio();
+		    relogio.definirHorario(17, 23, 15);
+		    // Testando a função de reiniciar o relogio para meia noite
+		    relogio.reiniciarMeiaNoite();
+		    assertEquals("00:00:00", relogio.obterHorario());
+		}
+	
+	   @Test
+	   public void testDefinirHorario() {
+	       Relogio relogio = new Relogio();
+	       // Testando a função de programar o relogio para determinado horário 
+	       relogio.definirHorario(15, 13, 0);
+	       assertEquals("15:13:00", relogio.obterHorario());
+	   }
+
 
 }
