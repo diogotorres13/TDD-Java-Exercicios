106a71345a05a9afc975c2378f2c206e100d9b76
diogotorres13 <104327677+diogotorres13@users.noreply.github.com>
2023-08-30 16:24:45 -0300

    criando pastas

---
+++ Teste/Relogio/RelogioTest.java
@@ -0,0 +1,14 @@
+package Relogio;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class RelogioTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
